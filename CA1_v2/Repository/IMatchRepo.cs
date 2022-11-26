3184fa234506eb7bf6aa80f14ac7bc6b24a98cb9
dacillo <johncarlodacillo123@gmail.com>
2022-11-26 16:57:19 +0000

    added repos actually

---
+++ CA1_v2/Repository/IMatchRepo.cs
@@ -0,0 +1,14 @@
+﻿using CA1_v2.Models;
+
+namespace CA1_v2.Repository
+{
+    public interface IMatchRepo
+    {
+        List<Match> GetFixtures();
+        Match GetFixture(int Id);
+
+        void CreateFixture(Match m);
+        void EditFixture(Match m);
+
+    }
+}
